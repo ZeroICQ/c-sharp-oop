78f1cc52b040802a13b843eb80fd6b39062c044a
Alexey Mogilyovkin <zeroicq@gmail.com>
2022-11-12 14:00:05 +1000

    Add top_level statement and lib

---
+++ StudyProject/hello_world_lib/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hello_world_lib
+{
+    class Program
+    {
+        public static void PrintHelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
