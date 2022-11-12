3642084a9c3a6628399de381e649246683ca0d0f
Alexey Mogilyovkin <zeroicq@gmail.com>
2022-11-12 14:23:27 +1000

    Add types

---
+++ StudyProject/types/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Объявление
+            int integerVariable;
+            // Инициализация
+            integerVariable = 2;
+            // Использование
+            integerVariable *= 2; // тоже самое что и integerVariable = integerVariable * 2
+            Console.WriteLine(integerVariable); // 4
+
+        }
+
+        static void Casting()
+        {
+            int integerVar = 2;
+            // целое число приводится к вещественному
+            double doubleVar = integerVar;
+            // вещественное к целому нет
+            // integerVar = doubleVar
+            
+            
+        }
+
+
+        static void ErrorExamples()
+        {
+            int myAge;
+            // Ошибка. Пеерменная не инициализирована
+            // Console.WriteLine(myAge);
+        }
+    }
+}
